9f92631f58b78a80aa42c8be0cb2509cbd0ce892
Mauricio Duarte <prussdev@gmail.com>
2019-10-09 15:24:03 -0300

    Componente loading

--- src/app/produtos/produtos.page.ts
+++ src/app/produtos/produtos.page.ts
@@ -1,8 +1,10 @@
+import { async } from '@angular/core/testing';
 import { API_CONFIG } from './../../config/api.config';
 import { ProdutoService } from './../../services/domain/produto.service';
 import { ProdutoDTO } from './../../models/produto.dto';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-produtos',
@@ -13,37 +15,63 @@ export class ProdutosPage implements OnInit {
 
   items: ProdutoDTO[];
 
+  isLoading = false;
+
   constructor(
     private route: ActivatedRoute,
     private produtoService: ProdutoService,
     private router: Router,
+    private loadCtrl: LoadingController,
   ) { }
 
   ngOnInit() {
 
     const id = +this.route.snapshot.paramMap.get('id');
 
+    this.presentLoading();
     this.produtoService.findByCategoria(id.toString())
       .subscribe(response => {
         this.items = response['content'];
+        this.dismiss();
         this.loadImageUrls();
       },
-        error => { });
+        error => {
+          this.dismiss();
+        });
   }
 
   loadImageUrls() {
-    for (var i=0; i<this.items.length; i++) {
+    for (var i = 0; i < this.items.length; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
           item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.id}-small.jpg`
         },
-        error => {});
+          error => { });
     }
   }
 
-  showDetail(produto_id){
+  showDetail(produto_id) {
     this.router.navigate(['/produto-detail/', produto_id]);
   }
+
+  async presentLoading() {
+    this.isLoading = true;
+    return await this.loadCtrl.create({
+      message: 'Aguarde...'
+    }).then(a => {
+      a.present().then(() => {
+        if (!this.isLoading) {
+          a.dismiss();
+        }
+      });
+    });
+  }
+
+  async dismiss() {
+    this.isLoading = false;
+    return await this.loadCtrl.dismiss();
+  }
+
 
 }
