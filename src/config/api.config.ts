b0affc83ccb7240402772f8a7c2a45514314f26d
Mauricio Duarte <prussdev@gmail.com>
2019-10-10 14:19:48 -0300

    Configuracao final para app

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://192.168.0.11:8080",
+    baseUrl: "https://curso-spring-ionic-mauriciopd.herokuapp.com",
     bucketBaseUrl: "https://curso-spring-ionic-mauriciopd.s3-sa-east-1.amazonaws.com"
 }
